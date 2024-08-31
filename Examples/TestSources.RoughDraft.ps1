df44776500b33878aacbef82fe801750ef44239b
James Brundage <+@noreply.github.com>
2024-08-31 14:45:07 -0700

    docs: Updating TestSources.RoughDraft.ps1
    Adding YUV and standard test source.

--- Examples/TestSources.RoughDraft.ps1
+++ Examples/TestSources.RoughDraft.ps1
@@ -5,6 +5,7 @@ Push-Location $OutputDirectory
 New-Media -TestSource rgbtestsrc -OutputPath "RGB.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource pal100bars -OutputPath "PAL100.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource pal75bars -OutputPath "PAL75.TestSource.png" -Resolution 1920x1080
-New-Media -TestSource smtpebars -OutputPath "SMPTE.TestSource.png" -Resolution 1920x1080
+New-Media -TestSource yuvtestsrc -OutputPath "YUV.TestSource.png" -Resolution 1920x1080
+New-Media -TestSource testsrc "TestSource.png" -Resolution 1920x1080
 
 Pop-Location
