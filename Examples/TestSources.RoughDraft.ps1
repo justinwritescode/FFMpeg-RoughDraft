4d807992b5db92471b418fb38be2d20c1b65c9d5
James Brundage <+@noreply.github.com>
2024-08-31 14:39:48 -0700

    docs: Updating TestSources.RoughDraft.ps1
    Simplifying

--- Examples/TestSources.RoughDraft.ps1
+++ Examples/TestSources.RoughDraft.ps1
@@ -2,9 +2,9 @@ $OutputDirectory = $PSScriptRoot
 
 Push-Location $OutputDirectory
 
-New-Media -TestSource rgbtestsrc -OutputPath "RGB_Test_Source.png" -Width 1920 -Height 1080 
-New-Media -TestSource pal100bars -OutputPath "PAL_100_Bars_Test_Source.png" -Width 1920 -Height 1080
-New-Media -TestSource pal75bars -OutputPath "PAL_75_Bars_Test_Source.png" -Width 1920 -Height 1080
-New-Media -TestSource smtpebars -OutputPath "SMPTE_Bars_Test_Source.png" -Width 1920 -Height 1080
+New-Media -TestSource rgbtestsrc -OutputPath "RGB.TestSource.png" -Resolution 1920x1080
+New-Media -TestSource pal100bars -OutputPath "PAL100.TestSource.png" -Resolution 1920x1080
+New-Media -TestSource pal75bars -OutputPath "PAL75.TestSource.png" -Resolution 1920x1080
+New-Media -TestSource smtpebars -OutputPath "SMPTE.TestSource.png" -Resolution 1920x1080
 
 Pop-Location
