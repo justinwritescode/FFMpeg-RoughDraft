fa3ace7d6c4333ee93b5e086c7202fcb21d27024
James Brundage <@github.com>
2024-08-31 14:18:07 -0700

    docs: Updating TestSources.RoughDraft.ps1

--- Examples/TestSources.RoughDraft.ps1
+++ Examples/TestSources.RoughDraft.ps1
@@ -1,18 +1,10 @@
-$testSourceOutputPath = New-Item -ItemType Directory -Path .\TestSources
+$OutputDirectory = $PSScriptRoot
 
-@(
-    New-Media -TestSource rgbtestsrc -OutputPath (
-        Join-Path -Path $testSourceOutputPath -ChildPath "RGB_Test_Source.png"
-    ) -Width 1920 -Height 1080 
-    New-Media -TestSource pal100bars -OutputPath (
-        Join-Path -Path $testSourceOutputPath -ChildPath "PAL_100_Bars_Test_Source.png"
-    ) -Width 1920 -Height 1080
-    New-Media -TestSource pal75bars -OutputPath (
-        Join-Path -Path $testSourceOutputPath -ChildPath "PAL_75_Bars_Test_Source.png"
-    ) -Width 1920 -Height 1080
-    New-Media -TestSource smtpebars -OutputPath (
-        Join-Path -Path $testSourceOutputPath -ChildPath "SMPTE_Bars_Test_Source.png"
-    ) -Width 1920 -Height 1080
-) | 
-    Add-Member NoteProperty CommitMessage "Adding Test Sources [skip ci]" -Force -PassThru
-    
+Push-Location $OutputDirectory
+
+New-Media -TestSource rgbtestsrc -OutputPath "RGB_Test_Source.png" -Width 1920 -Height 1080 
+New-Media -TestSource pal100bars -OutputPath "PAL_100_Bars_Test_Source.png" -Width 1920 -Height 1080
+New-Media -TestSource pal75bars -OutputPath "PAL_75_Bars_Test_Source.png" -Width 1920 -Height 1080
+New-Media -TestSource smtpebars -OutputPath "SMPTE_Bars_Test_Source.png" -Width 1920 -Height 1080
+
+Pop-Location
