f4fa3ebd92ffd5f58d558b9f646d383e48a0c862
James Brundage <+@noreply.github.com>
2024-09-01 11:25:16 -0700

    docs: Updating TestSources.RoughDraft.ps1
    Adding testsrc2

--- Examples/TestSources.RoughDraft.ps1
+++ Examples/TestSources.RoughDraft.ps1
@@ -7,5 +7,6 @@ New-Media -TestSource pal100bars -OutputPath "PAL100.TestSource.png" -Resolution
 New-Media -TestSource pal75bars -OutputPath "PAL75.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource yuvtestsrc -OutputPath "YUV.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource testsrc "TestSource.png" -Resolution 1920x1080
+New-Media -TestSource testsrc2 "TestSource2.png" -Resolution 1920x1080
 
 Pop-Location
