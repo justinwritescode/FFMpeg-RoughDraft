853b9df6322ad521c337e3948da9bec2a9e62d38
James Brundage <+@noreply.github.com>
2024-09-01 11:49:04 -0700

    docs: Adding mp4 test source examples

--- Examples/TestSources.RoughDraft.ps1
+++ Examples/TestSources.RoughDraft.ps1
@@ -8,5 +8,7 @@ New-Media -TestSource pal75bars -OutputPath "PAL75.TestSource.png" -Resolution 1
 New-Media -TestSource yuvtestsrc -OutputPath "YUV.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource testsrc "TestSource.png" -Resolution 1920x1080
 New-Media -TestSource testsrc2 "TestSource2.png" -Resolution 1920x1080
+New-Media -TestSource testsrc "TestSource.mp4" -Duration "00:00:01" -Resolution 1920x1080
+New-Media -TestSource testsrc2 "TestSource2.mp4" -Duration "00:00:01" -Resolution 1920x1080
 
 Pop-Location
