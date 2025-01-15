c83ec4a68fd5da8c5c5b29c40c4e137da0a1e2dd
James Brundage <+@noreply.github.com>
2025-01-15 01:00:50 -0800

    fix: Triaging Build Failures
    Using ErrorActionPreference within the TestSources Example file.

--- Examples/TestSources.RoughDraft.ps1
+++ Examples/TestSources.RoughDraft.ps1
@@ -2,6 +2,8 @@ $OutputDirectory = $PSScriptRoot
 
 Push-Location $OutputDirectory
 
+$global:ErrorActionPreference = $errorActionPreference = 'continue'
+
 New-Media -TestSource rgbtestsrc -OutputPath "RGB.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource pal100bars -OutputPath "PAL100.TestSource.png" -Resolution 1920x1080
 New-Media -TestSource pal75bars -OutputPath "PAL75.TestSource.png" -Resolution 1920x1080
